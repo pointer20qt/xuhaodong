2f6318279eab99522cab32c5993192b468dd7cca
CAT-X-sir <1605729007@qq.com>
2020-02-18 17:02:41 +0800

    2020/2/19 weak_ptr的实现及循环引用测试

--- weak_ptr的实现.h
+++ weak_ptr的实现.h
@@ -1,171 +1,148 @@
 #pragma once
-//模板化，可以接受任意类型
-//构造方法，赋值一个指针
-//析构方法，释放指针
-//不标准的复制构造(转移内部指针控制权)
-//不标准的复制赋值(转移内部指针控制权)
-//reset 释放之前的，获得新的指针
-//release()  释放控制权，返回指针
-//operator*() 模拟指针访问方法
+//模板化接受任意类型
+//构造方法 赋值一个指针
+//析构方法释放指针
+//不标准赋值构造（转移内部控制权）
+//不标准复制复制
+//reset 释放之前的，获得新指针
+//release()释放控制权返回指针
+//operator*()模拟指针访问方法
 //operator->() 模拟指针箭头式访问方法
-//get()  获得指针指向的指针
-//std::   
-namespace lan{
-	template<class T>
-	class L_autoPtr{
-		T* obj = nullptr;
-	public:
-		L_autoPtr(T* ptr) :obj(ptr){
-		}
-		L_autoPtr() = default;
-		~L_autoPtr(){
-			delete obj;
-		}
-		L_autoPtr(L_autoPtr<T>& ptr) :obj(ptr.release()){
+//get() 获得指针指向的指针
+template<class T>
+class X_autoptr{
+	T*obj = nullptr;
+public:
+	X_autoptr(T* ptr) :obj(ptr){
 
-		}
-		L_autoPtr& operator=(L_autoPtr<T>& ptr){
-			obj = ptr.release();
-			return *this;
-		}
-		void reset(T* newPtr = nullptr){
-			delete obj;
-			obj = newPtr;
-		}
-		T* release(){
-			auto temP = obj;
-			obj = nullptr;
-			return temP;
-		}
-		T& operator*(){
-			return *obj;
-		}
-		T* operator->(){
-			return obj;
-		}
-		T* get(){
-			return obj;
-		}
-		
-	};
-	template<class T> class L_uniquePtr{
-		T* obj = nullptr;
-	public:
-		L_uniquePtr(T* ptr) :obj(ptr){
-		}
-		L_uniquePtr(const T&) = delete;
-		L_uniquePtr() = default;
-		~L_uniquePtr(){
-			delete obj;
-		}
-		L_uniquePtr& operator=(const L_autoPtr<T>&) = delete;
-		L_uniquePtr(L_autoPtr<T>&& ptr) :obj(ptr.release()){
+	}
+	X_autoptr() = default;
+	~X_autoptr(){
+		delete obj;
+	}
+	X_autoptr(X_autoptr<T>& ptr){
+		obj = ptr.obj;
+		ptr.obj = nullptr;
+	}
+	X_autoptr& operator=(X_autoptr<T>&ptr){
+		delete obj;
+		obj = ptr.obj;
+		ptr.obj = nullptr;
+		return *this;
+	}
+	void reset(T *newPtr){
+		delete obj;
+		obj = newPtr;
+	}
+	T* release(){
+		auto temp = obj;
+		obj = nullptr;
+		return temp;
+	}
+	T& operator*(){
+		return *obj;
+	}
+	T* operator->(){
+		return obj;
+	}
+	T* get(){
+		return obj;
+	}
+};
 
-		}
-		L_uniquePtr& operator=(L_autoPtr<T>&& ptr){
-			obj = ptr.release();
-			return *this;
-		}
-		void reset(T* newPtr = nullptr){
-			delete obj;
-			obj = newPtr;
-		}
-		T* release(){
-			auto temP = obj;
-			obj = nullptr;
-			return temP;
-		}
-		T& operator*(){
-			return *obj;
-		}
-		T* operator->(){
-			return obj;
-		}
-		T* get(){
-			return obj;
-		}
-	};
-	//资源计数类
+namespace Xu{
 	template<class T>
 	class Ref{
-		//当前资源计数
-		int r_count=0;
-		T* object=nullptr;
+		T* obj = nullptr;
+		int r_cout = 0;
+		int w_cout = 0;
 	public:
-		Ref(T* target):object(target){
-			++r_count;
-		}
-		inline void increase(){
-			r_count++;
+		Ref() = default;
+		Ref(T* target) :obj(target), r_cout(1), w_cout(1){
+
 		}
 		inline void reduce(){
-			r_count--;
-			if (r_count == 0){
-				delete object;
-				delete this;
+			r_cout--;//因为这个引用对象建立时就置为1
+			if (r_cout == 0){
+				delete obj;//释放指向对象
+				obj = nullptr;
+				//当w也为0才释放引用
+				if (w_cout == 0){
+					delete this;//释放当前引用对象
+				}
 			}
 		}
-		//获得对对象
-		T* get(){
-			return object;
+		inline void w_reduce(){
+			w_cout--;//因为这个引用对象建立时就置为1
+			if (w_cout == 0 && r_cout == 0){//强弱都没有才释放
+				delete this;//释放当前引用对象
+			}
 		}
-		int getCount(){
-			return r_count;
+		inline void increase(){
+			r_cout++;
+		}
+		inline void w_increase(){
+			w_cout++;
+		}
+		T* get(){
+			return obj;
+		}
+		int getCout(){
+			return r_cout;
 		}
 	};
-	//shared智能指针需要的方法
-	/*
-	无参构造，传递指针构造，拷贝构造，移动构造，拷贝赋值，移动赋值,交换，析构函数
-	reset(T*)替换对象。 reset() 销毁对象
-	operator*() operator->()
-	get()获得裸指针
-	use_count() 获得引用计数
-	unique() 判断当前是否唯一  use_count()==1
-	operator bool() 是否关联对象
-	*/
-	template<class T> class LSharedPtr{
-		Ref<T>* ref=nullptr;
+	//提前声明 否则会报Xu不是模板的错误
+	template<class T> class Xweak_ptr;
+	template<class T>
+	class Xshared_ptr{
+		Ref* ref = nullptr;
+		friend class Xweak_ptr<T>;
 	public:
-		LSharedPtr() = default;
-		~LSharedPtr(){
+		Xshared_ptr() = default;
+		Xshared_ptr(Ref* newRef){
+			ref = newRef;
 			if (ref){
-				ref->reduce();
+				ref->increase();
 			}
 		}
-		LSharedPtr(T* newP){
+		Xshared_ptr(T* newP){
 			ref = new Ref<T>(newP);
 		}
-		LSharedPtr(const LSharedPtr& other){
-			ref = other.ref;
+		~Xshared_ptr(){
+			if (ref){
+				ref->reduce();
+			}
+		}
+		Xshared_ptr(const Xshared_ptr<T> &other){//other为被拷对象
+			ref = other->ref;
 			if (ref){
 				ref->increase();
 			}
 		}
-		LSharedPtr(LSharedPtr&& other){
-			swap(ref,other.ref);
+		Xshared_ptr(const Xshared_ptr<T> &&other){//移动构造直接可以交换函数
+			swap(ref, other->ref);
 		}
-		LSharedPtr& operator=(const LSharedPtr& other){
+		Xshared_ptr& operator=(const Xshared_ptr<T>&other){//1.返回值引用才能操作 
 			if (ref){
-				ref->reduce();
+				ref->reduce();//指针被赋值后引用内个数减一或析构对象
 			}
-			ref = other.ref;
+			ref = other->ref;
 			if (ref){
 				ref->increase();
 			}
-			return *this;
 		}
-		LSharedPtr& operator=(LSharedPtr&& other){
+		Xshared_ptr& operator=(const Xshared_ptr<T>&&other){//把传进来的搞空喽
 			if (ref){
 				ref->reduce();
 			}
-			ref = other.ref;
-			other.ref = nullptr;
+			ref = other->ref;
+			other.ref = nullptr;//指针没丢传给另一个对象
 			return *this;
 		}
-		void swap(LSharedPtr& other){
-			swap(ref,other.ref);
+		void swap(Xshared_ptr<T> &other){
+			swap(ref, other->ref);
 		}
-		void reset(T* target=nullptr){
+		void reset(T* target = nullptr){
 			if (ref){
 				ref->reduce();
 			}
@@ -175,6 +152,7 @@ namespace lan{
 			else{
 				ref = nullptr;
 			}
+
 		}
 		T& operator*(){
 			if (ref){
@@ -202,7 +180,7 @@ namespace lan{
 		}
 		int use_count(){
 			if (ref){
-				return ref->getCount();
+				return ref->getCout();
 			}
 			else{
 				return 0;
@@ -210,19 +188,70 @@ namespace lan{
 		}
 		bool unique(){
 			if (ref){
-				return ref->getCount() == 1;
+				return ref->getCout() == 1;
+			}
+			else{
+				return false;
+			}
+
+		}
+	};
+
+
+	//Xweak_ptr的实现
+	template<class T>
+	class Xweak_ptr{
+		Ref<T>* ref = nullptr;
+	public:
+		Xweak_ptr() = default;
+		Xweak_ptr(const Xshared_ptr<T>& other) :ref(other.ref){
+			if (ref){
+				ref->w_reduce;
+			}
+		}
+		Xweak_ptr(const Xweak_ptr&other) :ref(other.ref){
+			if (ref){
+				ref->w_increase();
+			}
+		}
+		Xweak_ptr(Xweak_ptr&& other){
+			swap(ref, other.ref);
+		}
+		void operator=(const Xweak_ptr& other){
+			if (ref){
+				ref->w_reduce();
+			}
+			ref = other.ref;
+			if (ref){
+				ref->w_increase();
+			}
+		}
+		int use_cout(){
+			if (ref){
+				return ref->getCout();
+			}
+			else{
+				return 0
+			}
+		}
+		bool expired(){
+			if (ref){
+				return ref->getCout() > 0;
 			}
 			else{
 				return false;
 			}
 		}
-		//判断当前只能指针是否和对象关联
-		operator bool(){
-			return ref != nullptr;
+		Xshared_ptr<T> lock(){
+			Xshared_ptr<T> tem(ref);
+			return tem;
 		}
-	};
-	//可以指定一个引用计数对象，在需要时
-	class LweakPtr{
-
+		void swap(Xweak_ptr<T>& other){
+			swap(ref, other.ref);
+		}
+		void reset(){
+			ref->w_increase();
+			ref = nullptr;
+		}
 	};
 }
